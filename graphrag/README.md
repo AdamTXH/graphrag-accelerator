85fe80ef66bb3294a06d5c4fa0d44a4b07d1d7ef
AdamTXH <xue.hao.tay@intel.com>
2024-07-10 16:20:49 +0800

    Code to set up ingestion and query

---
+++ graphrag/README.md
@@ -0,0 +1,32 @@
+To run the graph rag library:
+
+`conda create -n graphrag python=3.10`
+
+`pip install graphrag`
+
+`python -m graphrag.index --init --root ./graphrag`
+
+- output/, prompts/, settings.yaml will be created. Insert your openai api key in `GRAPHRAG_API_KEY`.
+
+To create knowledge graph:
+- Configure settings in settings.yaml, api type, models, chunk size, file type, etc.
+
+`python -m graphrag.index --root ./graphrag`
+
+- Results will be saved into ./graphrag/output/datetime. Contains some log files and multiple .parquet files.
+
+To Query:
+
+ - Global mode:
+
+`
+python -m graphrag.query \
+--root ./graphrag \
+--method global \
+"What are the three ai engines?"
+`
+
+
+
+Supports local models.
+Can be used locally or with azure resource group.
---
+++ graphrag/query.py
@@ -0,0 +1,26 @@
+from graphrag.query.cli import run_local_search
+import argparse
+import time
+# Example usage
+
+def main():
+    parser = argparse.ArgumentParser(description='Process a query string.')
+    parser.add_argument('query', type=str, help='The query string to process')
+    
+    args = parser.parse_args()
+    
+    query = args.query
+    print(f"Received query: {query}")
+    
+    start_time = time.time()
+    run_local_search(data_dir="/home/adamtay/graphrag-accelerator/graphrag/output/20240710-131106/artifacts",
+                    root_dir="/home/adamtay/graphrag-accelerator/graphrag",
+                    community_level=2,
+                    response_type="clear and conciose", # Free form text describing the response type and format, can be anything, e.g. Multiple Paragraphs, Single Paragraph, Single Sentence, List of 3-7 Points, Single Page, Multi-Page Report
+                    query=query)
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time} seconds")
+
+
+if __name__ == "__main__":
+    main()
